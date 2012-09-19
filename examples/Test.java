275b5151fe5f0fc084705e80904b77f25760c436
Glenn Downing <downing@cs.utexas.edu>
2012-09-19 16:19:43 -0500

    another commit

--- examples/Test.java
+++ examples/Test.java
@@ -4,37 +4,21 @@
 
 import java.util.Arrays;
 
+class A extends Exception {}
+class B extends A {}
+
+class C {
+    void f () throws B {}}
+
+class D {
+    void f () throws A {}}
+
 final class Test {
-    private static void f (int j) {
-        j = 3;}
 
-    private static void g (String t) {
-        t = "def";}
-
-    private static void h (int[] b) {
-        b[1] = 5;}
-
-    public static void main (String[] args) {
+    public static void main (String[] args) throws A {
         System.out.println("Arguments.java");
 
-        {
-        int i = 2;
-        f(i);
-        assert i == 2;
-        }
-
-        {
-        String s = "abc";
-        g(s);
-        assert s == "abc";
-        }
-
-        {
-        int[] a = {2, 3, 4};
-        h(a);
-        assert a != new int[]{2, 5, 4};
-        assert !a.equals(new int[]{2, 5, 4});
-        assert Arrays.equals(a, new int[]{2, 5, 4});
-        }
+        C x = new C();
+        x.f();
 
         System.out.println("Done.");}}
